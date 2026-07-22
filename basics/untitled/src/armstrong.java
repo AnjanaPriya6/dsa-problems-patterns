82e18cfa030da422c70b66e840fb9cc3f61e8725
anjanapriya.konapala6@gmail.com <anjanapriya.konapala6@gmail.com>
2026-07-22 10:33:43 +0530

    pushing the basic problem sets

---
+++ basics/untitled/src/armstrong.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class armstrong {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int length = String.valueOf(n).length();
+        int sum = 0;
+        while(n>0){
+           int lastDigit = n%10;
+           sum = (int) (sum+Math.pow(lastDigit,length));
+           n = n/10;
+        }
+        System.out.println(temp == sum);
+    }
+}
